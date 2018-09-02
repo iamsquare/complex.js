457a967d833f142653120df26f653c38071bf2c5
squaremarco <sq@iamsquare.it>
2018-09-02 04:44:02 +0200

    Added a Helpers test

--- tests/helpers.spec.ts
+++ tests/helpers.spec.ts
@@ -8,16 +8,19 @@ describe('Helpers', () => {
 
   const c: Cartesian = { x: 10, y: 20 };
   const p: Polar = { r: 11, p: Math.PI };
+  const u: any = undefined;
 
   describe('Helper functions', () => {
     test('isCartesian(object)', () => {
       expect(isCartesian(c)).toBeTruthy();
       expect(isCartesian(p)).toBeFalsy();
+      expect(isCartesian(u)).toBeFalsy();
     });
 
     test('isPolar(object)', () => {
       expect(isPolar(c)).toBeFalsy();
       expect(isPolar(p)).toBeTruthy();
+      expect(isPolar(u)).toBeFalsy();
     });
   });
 });
