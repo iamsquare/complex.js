5be6f57bc8d4d21a8e13f5d1dfc252014982e680
squaremarco <squaremarco@gmail.com>
2019-02-16 13:56:08 +0100

    Changed how the library is transpiled and bundled

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,19 @@
 import pkg from './package.json';
 
-import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 
 module.exports = [
   {
-    input: 'temp/index.js',
-    output: {
-      file: pkg.main,
-      sourcemap: true,
-      format: 'cjs',
-      exports: 'named'
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel(),
-      terser({
-        mangle: false
-      })
-    ]
-  },
-  {
-    input: 'temp/index.js',
+    input: 'dist/lib/index.js',
     output: {
       file: pkg.module,
       sourcemap: true,
-      format: 'esm'
+      format: 'es'
     },
-    plugins: [resolve(), commonjs(), babel()]
+    plugins: [
+      commonjs(),
+      resolve()
+    ]
   }
 ];
